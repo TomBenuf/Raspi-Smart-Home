d5297bf992592a14f49014547fc304d86ad79730
Tom <tom-barnowsky@hls-anlagenbau.de>
2018-11-08 21:50:38 +0100

    Erster PHP Code
    index.html durch index.php ersetzt. Sowieso nötig.
    PHP lädt jetzt XML in KLasse $xml
    erstellt für jedes device aus dem XML einen Button der bis jetzt noch keine Aktion hat.

---
+++ www/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!-- Tom Barnowsky, Nils Rothenburger, Robin Schmidt - 2018-11-03
+   - Netzwerkgestützte Smart-Home Steuerung via Raspberry Pi
+
+   - Dies ist die INDEX HTML-Datei. -->
+
+<html>
+<head>
+
+</head>
+<body>
+	<h3> IT WORKS! </h3>
+	This is the example index.html for the raspi-smart-home project.<br>
+	Plus it has some php now :O
+	<p>
+
+<?php
+
+/* Läd XML und erstellt für jedes /devices/device einen Button.
+ * Bis jetzt aber noch ohne Aktion */
+
+$xml = simplexml_load_file('status.xml');
+
+echo "<form action='' method='POST'>";
+
+foreach ($xml->device as $device){
+	echo "<input type='button' value=".$device->name.">";
+	}
+echo "</form>";
+
+?>
+</body>
+</html>
