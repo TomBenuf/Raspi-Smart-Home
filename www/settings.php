252bb9632b858c973f4559bce230dcc3158b50fe
Tom <tom-barnowsky@hls-anlagenbau.de>
2018-11-23 19:19:21 +0100

    settings.php 	Vergessen zum letzten Commit hinzuzufügen. 	Änderungen siehe letzte Commit

---
+++ www/settings.php
@@ -0,0 +1,48 @@
+<!Doctype html>
+<!-- Tom Barnowsky, Nils Rothenburger, Robin Schmidt - 2018-11-23
+   - Netzwerkgestütze Smart-Home Steuerung via Raspberry Pi
+
+   - Dies ist die Settings datei um die Oberfläche einzustellen. -->
+
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" type="text/css" href="format.css" />
+	<Title>Einstellungen</title>
+</head>
+<body>
+	<h1>Einstellungen für Buttons</h1>
+	<form action="" method="POST">
+
+	</form>
+
+<?php
+
+
+
+
+
+?>
+
+	<form action="index.php" target="_self">
+	<input type="submit" value="Zurück">
+	</form>
+</body>
+</html>
+
+<!-- Angefangene Arbeiten an index.php zum vlt verwenden hier
+
+
+echo "<p><input type='button' name='ferstellen' value='Erstellen' onClick='1' id='button2'></p>";
+if($_POST['ferstellen']=='1')
+{
+	echo "<p>Bitte neuen Namen eingeben: <input type='text' name='fneuname'></p>
+	<p>Bitte Passwort für Berechtigung eingeben: <input type='password' name='fpassw'></p>";
+	$neuname=$_POST['fneuname'];
+	$passw=$_POST['fpassw'];
+}
+if($passw=="")
+{echo"<input type='button' value='$neuname'id='button1'>";
+
+	}
+-->
