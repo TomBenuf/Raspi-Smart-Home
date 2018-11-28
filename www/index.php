396d79c7883babbfe7e00d78699c1df287cdb499
Tom <tom-barnowsky@hls-anlagenbau.de>
2018-11-28 21:27:24 +0100

    index.php 	Holiday Funktion aktiviert 	Timer Funktion aktiviert 	checked wenn in xml gesetzt aktiviert 	!! immernoch Start Warnung
    Rechteverwaltung geändert. Jetzt Gruppen lese/schreibrecht.

--- www/index.php
+++ www/index.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <!-- Tom Barnowsky, Nils Rothenburger, Robin Schmidt - 2018-11-23
    - Netzwerkgestützte Smart-Home Steuerung via Raspberry Pi
 
@@ -6,61 +6,87 @@
 
 <html>
 <head>
-
-<title>
-Raspi Smart-Home
-</title>
+<title>Raspi Smart-Home</title>
 <link rel="stylesheet" type="text/css" href="format.css" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
 
 </head>
 <body>
 	<p> <h1>Ihre Smart-Home Steuerung für unterwegs.</h1> </p>
+	
 <?php
-/* Läd XML und erstellt für jedes /devices/device einen Button.
- * Bis jetzt aber noch ohne Aktion */
+// Läd XML und erstellt für jedes /devices/device einen Button.
+
+echo "<form action='' method='POST'><p>
+	Einschaltzeit eingeben: <input type='text' name='fanfz' value=".$_POST['fanfz']."><br>
+	Ausschaltzeit eingeben: <input type='text' name='fendz' value=".$_POST['fendz']."></p><p>";
 
 $xml = simplexml_load_file('status.xml');
-echo "<form action='' method='POST'>
-	<input type='button' value='Urlaubsmdous' name='holiday' onclick='1'>
-	<input type='text' name='fanfz' value=".$_POST['fanfz'].">
-	<input type='text' name='fendz' value=".$_POST['fendz']."><p>";
-$anfz=$_POST['fanfz'];
-$endz=$_POST['fendz'];
 
-	//Erzeuge Checkbox für jedes Gerät
-	foreach ($xml->device as $device){
-	echo $device->name."<input type='checkbox' id='check' name='fcheck".$device['id']."'><br>";
-	}
-
-echo "<input type='submit' value='ok'></form>";
-//print_r($_POST);
-
+if(isset($_POST['submit'])){
+	//echo "Submitted";
+	
 	//Schreibt Werte die in Chekcboxen ausgewählt wurden in $xml
 	foreach($xml->device as $device1){
-	$indent = "fcheck".$device1['id'];
-	//echo $indent."&nbsp";
+		$indent = "fcheck".$device1['id'];
 
-		If($_POST[$indent]){
-		$device1->status = 'on';
-		//echo $device1->name.$device1->status."<br>";
+		If(isset($_POST[$indent])){
+			$device1->status = 'on';
+
+			If(isset($_POST['fanfz'])){
+				$xml->device->timer->on = strtotime($_POST['fanfz']);
+			}
+
+			If(isset($_POST['fendz'])){
+				$xml->device->timer->off = strtotime($_POST['fendz']);
+			}
 		}
 
 		else{
-		$device1->status = 'off';
-		//echo $device1->name.$device1->status."<br>";
+			$device1->status = 'off';
 		}
 	}
 
-//Speichert $xml in Datei
-file_put_contents('status.xml', $xml->asXML());
-//print_r($xml);
+	If(isset($_POST['holiday'])){
+		$xml->holiday = 'on';
+	}
+
+	else{
+		$xml->holiday = 'off';
+	}
+
+	$anfz=$_POST['fanfz'];
+	$endz=$_POST['fendz'];
+
+	//Speichert $xml in Datei
+	file_put_contents('status.xml', $xml->asXML());
+}
+
+//Erzeuge Checkbox für jedes Gerät
+foreach($xml->device as $device){
+	echo $device->name."<input type='checkbox' id='check' name='fcheck".$device['id']."'";
+
+	If($device->status == 'on'){
+		echo "checked";
+	}
+
+	echo "><br>";
+}
+
+echo "Urlaubsmodus <input type='checkbox' name='holiday'";
+
+if($xml->holiday == 'on'){
+	echo "checked";
+}
+
+echo "></p><input type='submit' name='submit' value='OK'></form>";
+
+//print_r($_POST);
+
 ?>
-<form action="settings.php" target="_self">
-<input type="submit" value="Einstellungen">
-</form>
 
-
-
+	<form action="settings.php" target="_self">
+		<input type="submit" value="Einstellungen">
+	</form>
 </body>
 </html>
